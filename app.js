71f5b306696de612b9b6939f09e3f8cbed5cc14f
roryf <rory.fitzgerald@ntnu.no>
2024-05-01 12:36:41 +0200

    added python listener for mqtt

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const path = require('path');
+const { spawn } = require('child_process');
 const app = express();
 const port = 3000;
+
+// Run Python script
+const python = spawn('./venv/bin/python', ['pythonas.py']);
+// Collect data from script
+python.on('error', (error) => {
+  console.error('Error starting Python script:', error);
+});
+let scriptOutput = '';
+python.stdout.on('data', function (data) {
+  console.log('Python script output:', data.toString());
+  try {
+    scriptOutput = JSON.parse(data.toString());
+  } catch (error) {
+    console.error('Error parsing Python script output:', error);
+  }
+});
+// In case of error
+python.stderr.on('data', (data) => {
+ console.error(`stderr: ${data}`);
+});
+
+// End process
+python.on('close', (code) => {
+ console.log(`child process close all stdio with code ${code}`);
+});
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/rfid', (req, res) => {
+  res.send(scriptOutput);
 });
 
 app.listen(port, () => {
---
+++ pythonas.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import traceback
+import json
+try:
+    mqtt_broker="broker.hivemq.com"
+    mqtt_topic="mortensineRfid/scan"
+
+    def on_message(client, userdata, message):
+        blob=message.payload.decode()
+        parts=blob.split(", ")
+        rfid=parts[0].split(": ")[1]
+        if parts[1].split(": ")[1]=="LOW":
+            in_out=True
+        else:
+            in_out=False
+        print(json.dumps({"rfid": rfid, "in_out": in_out}), flush=True)        
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.connect(mqtt_broker,1883)
+    client.subscribe(mqtt_topic)
+    client.loop_forever()
+except Exception:
+    traceback.print_exc()
