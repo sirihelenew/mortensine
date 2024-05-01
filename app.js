3e79fbfb5d4a1371cb79d5963aa7d673e3b9bc88
sirihelenew <sirihwa@stud.ntnu.no>
2024-05-01 14:00:24 +0200

    cascm

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./mortensine-a58cb-firebase-adminsdk-7r4ub-c075dab8d4.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://mortensine-a58cb.firebaseio.com'
+});
+
+const db = admin.firestore();
+const storage = admin.storage();
+
+
 const express = require('express');
 const path = require('path');
 const { spawn, exec } = require('child_process');
@@ -24,6 +36,35 @@ exec('git pull', (error, stdout, stderr) => {
     console.log('Python script output:', data.toString());
     try {
       scriptOutput = JSON.parse(data.toString());
+      const rfidTag = scriptOutput.rfid;
+      const status=scriptOutput.in_out;
+      // Check if the RFID tag is associated with a user...
+      const userRef = db.collection('brukere').where('rfidTag', '==', rfidTag);
+      userRef.get()
+      .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+              const userID = querySnapshot.docs[0].id;
+              const tid = admin.firestore.Timestamp.now();
+              const metode = 'RFID';
+              db.collection('Innlogginger').add({
+                  userID,
+                  tid,
+                  metode,
+                  status // or 'ut' depending on your logic
+              })
+              .then(() => {
+                  //res.json({ status: 'success' });
+                  console.log('Document updated successfully');
+              })
+              .catch((error) => {
+                  //res.json({ status: 'error', error: 'Error updating document: ' + error });
+                  console.error("Error updating document: ", error);
+              });
+          } else {
+              //res.json({ status: 'error', error: 'User not found' });
+              console.log('User not found');
+          }
+      });
     } catch (error) {
       console.error('Error parsing Python script output:', error);
     }
@@ -41,9 +82,6 @@ exec('git pull', (error, stdout, stderr) => {
   // Serve static files from the public directory
   app.use(express.static(path.join(__dirname)));
 
-  app.get('/rfid', (req, res) => {
-    res.send(scriptOutput);
-  });
 
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
