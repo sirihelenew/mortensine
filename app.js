5af31cd6e4ddb4090a063822d3b92f8635c41535
roryf <rory.fitzgerald@ntnu.no>
2024-05-01 13:23:22 +0200

    git pull on startup

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
 const express = require('express');
 const path = require('path');
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 const app = express();
 const port = 3000;
 
-// Run Python script
-const python = spawn('./venv/bin/python', ['pythonas.py']);
-// Collect data from script
-python.on('error', (error) => {
-  console.error('Error starting Python script:', error);
-});
-let scriptOutput = '';
-python.stdout.on('data', function (data) {
-  console.log('Python script output:', data.toString());
-  try {
-    scriptOutput = JSON.parse(data.toString());
-  } catch (error) {
-    console.error('Error parsing Python script output:', error);
+// Pull the latest code from the Git repository
+exec('git pull', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error pulling code from Git: ${error}`);
+    return;
   }
-});
-// In case of error
-python.stderr.on('data', (data) => {
- console.error(`stderr: ${data}`);
-});
+  console.log(`Git pull stdout: ${stdout}`);
+  console.error(`Git pull stderr: ${stderr}`);
 
-// End process
-python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
-});
+  // Run Python script
+  const python = spawn('./venv/bin/python', ['pythonas.py']);
+  // Collect data from script
+  python.on('error', (error) => {
+    console.error('Error starting Python script:', error);
+  });
+  let scriptOutput = '';
+  python.stdout.on('data', function (data) {
+    console.log('Python script output:', data.toString());
+    try {
+      scriptOutput = JSON.parse(data.toString());
+    } catch (error) {
+      console.error('Error parsing Python script output:', error);
+    }
+  });
+  // In case of error
+  python.stderr.on('data', (data) => {
+   console.error(`stderr: ${data}`);
+  });
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname)));
+  // End process
+  python.on('close', (code) => {
+   console.log(`child process close all stdio with code ${code}`);
+  });
 
-app.get('/rfid', (req, res) => {
-  res.send(scriptOutput);
-});
+  // Serve static files from the public directory
+  app.use(express.static(path.join(__dirname)));
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  app.get('/rfid', (req, res) => {
+    res.send(scriptOutput);
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
 });
 
 module.exports = app;
