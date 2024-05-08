e0174baba756b9a3ebd77c636a6998fc9d47fdd4
roryf <rory.fitzgerald@ntnu.no>
2024-05-08 17:32:04 +0200

    idk man

---
+++ funcs/mqtt.js
@@ -0,0 +1,84 @@
+const mqtt= require('mqtt');
+const {db,storage} = require('./firebase');
+const logger = require('./logger');
+const { spawn } = require('child_process');
+const admin = require('firebase-admin');
+
+
+
+const mqttBroker = 'mqtt://broker.hivemq.com';
+const mqttTopic = 'mortensineRfid/scan';
+const mqttOutboundTopic = 'mortensinaActivate/go';
+
+var client = mqtt.connect(mqttBroker);
+
+client.on('connect', function () {
+    client.subscribe(mqttTopic, function (err) {
+      if (err) {
+        logger.error('Error subscribing to topic: ', err);
+      }
+    });
+  });
+
+
+
+client.on('message', function (topic, message) {
+    if (topic === mqttTopic) {
+      const blob = message.toString();
+      const parts = blob.split(", ");
+      const rfidTag = parts[0].split(": ")[1];
+      const status = parts[1].split(": ")[1] === "LOW";
+      console.log(JSON.stringify({ rfid: rfidTag, in_out: status }));
+      client.publish(mqttOutboundTopic, 'true');
+  
+      // Check if the RFID tag is associated with a user...
+      const userRef = db.collection('brukere').where('rfidTag', '==', rfidTag);
+      userRef.get()
+      .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+              const userID = querySnapshot.docs[0].id;
+              const navn=querySnapshot.docs[0].data().fornavn+' '+querySnapshot.docs[0].data().etternavn;
+              const tid = admin.firestore.Timestamp.now();
+              const metode = 'RFID';
+              const sound = spawn('./venv/bin/python', ['playsound.py', navn]);
+              logger.info("Spawned sound")
+              sound.stdout.on('data', (data) => {
+                logger.info(`stdout: ${data}`);
+              });
+              sound.stderr.on('data', (data) => {
+                logger.error(`stderr: ${data}`);
+               });
+              sound.on('error', (error) => {
+                logger.error('Error starting Python script:', error);
+              });
+              db.collection('Innlogginger').add({
+                  userID,
+                  tid,
+                  metode,
+                  status // or 'ut' depending on your logic
+              })
+              .then(() => {
+                  //res.json({ status: 'success' });
+                  logger.info('Document updated successfully');
+              })
+              .catch((error) => {
+                  //res.json({ status: 'error', error: 'Error updating document: ' + error });
+                  logger.error("Error updating document: ", error);
+              });
+          } else {
+              //res.json({ status: 'error', error: 'User not found' });
+              logger.info('User not found');
+              const python = spawn('./venv/bin/python', ['play_sound.py', NaN]);
+          }
+      });
+    }
+  });
+  
+client.on('error', function (error) {
+    logger.error('MQTT client error: ', error);
+    setTimeout(() => {
+      client.end();
+      client = mqtt.connect(mqttBroker);
+    }, 5000); // delay the reconnection for 5 seconds
+  });
+  
---
+++ routes/upload.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+
+
+
+const localstorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      const dir = './uploads/' + req.body.username;
+      try {
+        fs.mkdirSync(dir, { recursive: true });
+        logger.info(`Directory ${dir} created successfully`);
+      } catch (error) {
+        logger.error(`Error creating directory ${dir}: ${error}`);
+      }
+      cb(null, dir); // Destination folder
+    },
+    filename: function(req, file, cb) {
+      const filename = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+      logger.info(`File to be uploaded: ${filename}`);
+      cb(null, filename); // Naming the file
+    }
+  });
+  
+const upload = multer({ 
+    storage: localstorage,
+    limits: { fileSize: 10000000 }  // 10MB
+  });
+
+router.post('/', upload.single('mp3File'), (req, res) => {
+    if (req.file) {
+      logger.info(`File ${req.file.filename} uploaded successfully`);
+      res.send('File uploaded successfully');
+    } else {
+      const message = 'Error uploading file';
+      logger.error(message);
+      res.status(500).send(message);
+    }
+  });
+
+module.exports = router;
