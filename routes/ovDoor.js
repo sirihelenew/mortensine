9aec5d68229f5883f1bed00ccc2437567f99d80b
soddlover <rory.fitzg@hotmail.com>
2024-05-09 13:19:49 +0200

    doesnt poll OV all the time

--- routes/ovDoor.js
+++ routes/ovDoor.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+var latestStatus = null;
+var fetchError = null;
+
+function fetchStatus() {
     fetch('https://omegav.no/api/dooropen.php')
         .then(response => response.json())
-        .then(data => res.json(data))
-        .catch(error => res.status(500).json({ error: error.toString() }));
+        .then(data => {
+            latestStatus = data;
+            fetchError = null;
+        })
+        .catch(error => {
+            fetchError = error.toString();
+        });
+}
+
+// Fetch status immediately on server start
+fetchStatus();
+
+// Then fetch status every 60 seconds
+setInterval(fetchStatus, 60000);
+
+router.get('/', (req, res) => {
+    if (fetchError) {
+        res.status(500).json({ error: fetchError });
+    } else if (latestStatus) {
+        res.json(latestStatus);
+    } else {
+        res.status(500).json({ error: 'No status available' });
+    }
 });
 
 module.exports = router;
