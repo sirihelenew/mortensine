0bed7e49b0e7e4901f053dc3f08bec1a01fef1a5
soddlover <rory.fitzg@hotmail.com>
2024-05-09 19:38:59 +0200

    https maybe?

--- app.js
+++ app.js
@@ -43,7 +43,13 @@ app.use("/logs", logsRouter);
 app.use("/upload", uploadRouter);
 app.use("/order", orderRouter);
 
-
+app.use(function(req, res, next) {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 try {
   const stdout = execSync('./setup.sh');
