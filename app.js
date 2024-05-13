f12414204f5f7615f5f087eee6f9a5335e3b0010
soddlover <rory.fitzg@hotmail.com>
2024-05-13 18:23:14 +0200

    .html fallback

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.use(morganLogger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'),{
+  extensions: ['html']
+}));
 
 
 app.use('/', indexRouter);
